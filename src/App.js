ca91a540e98979cf8de38aa5c272e7e3dd7a92eb
abimcintyre <abimcintyre@performanceinpeople.co.uk>
2023-01-25 09:25:08 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This is an{" "}
-        <a
-          href="https://github.com/abimcintyre/week5"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced
-        </a>{" "}
-        project coded by Abi McIntyre
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This is an{" "}
+          <a
+            href="https://github.com/abimcintyre/week5"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced
+          </a>{" "}
+          project coded by Abi McIntyre
+        </footer>
+      </div>
     </div>
   );
 }
