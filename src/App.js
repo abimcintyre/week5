f89c8f4c0ee368cebd4ecd0899ab44c366137930
abimcintyre <abimcintyre@performanceinpeople.co.uk>
2023-01-25 09:13:21 +0000

    Added source code URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This is an{" "}
+        <a
+          href="https://github.com/abimcintyre/week5"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open sourced
+        </a>{" "}
+        project coded by Abi McIntyre
+      </footer>
+    </div>
+  );
 }
