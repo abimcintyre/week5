1febd211ed06c353bcb906fcc61b40a9feb6cd57
abimcintyre <abimcintyre@performanceinpeople.co.uk>
2023-02-01 09:27:53 +0000

    Added forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,6 +6,10 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+ 
+useEffect (() => {
+  setLoaded(false);  
+}, [props.coord]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -16,18 +20,27 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
         </div>
       </div>
     );
   } else {
     let lat = props.coord.lat;
     let lon = props.coord.lon;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=3499ef150985eccadd080ff408a018df&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=bf54175800a55e59e6c4d6461deeef12&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
+
     return null;
   }
 }
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,20 +1,27 @@
 import React from "react";
 
 export default function WeatherForecastDay(props) {
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
   return (
     <div>
       <div className="WeatherForecast-day">
-        <strong>{props.data.dt}</strong>
+        <strong>{day()}</strong>
       </div>
       <div className="WeatherForecast-icon">
         <img src={props.image} alt="Icon" />
       </div>
       <div className="WeatherForecast-temps">
         <span className="WeatherForecast-max">
-          {Math.round(props.data.temp.max)}°
+          {Math.round(props.data.temp.max)}°C
         </span>{" "}
         <span className="WeatherForecast-min">
-          {Math.round(props.data.temp.min)}°
+          {Math.round(props.data.temp.min)}°C
         </span>
       </div>
     </div>
